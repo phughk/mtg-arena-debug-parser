370ec75b3976e61e628df1ec8929f561bb2a1cc7
Hugh Kaznowski <phkaznowski@gmail.com>
2024-03-19 01:24:27 +0000

    Some weird code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::collections::BTreeMap;
 use std::io::BufRead;
 use log::info;
@@ -6,72 +7,90 @@ use rayon::prelude::*;
 
 const INITIAL_SIZE: usize = 200_000;
 
-fn parse_file() -> BTreeMap<usize, u32> {
+// MacOS: /Users/hugh/Library/ApplicationSupport/com.wizards.mtga/Logs/Logs/UTC_Log - *.log
+
+pub struct LogFileData {
+    metadata: Metadata,
+    decks: Vec<Deck>,
+    games: Vec<Game>,
+}
+
+pub struct Metadata {}
+pub struct Deck {}
+pub struct Game {}
+
+fn parse_file() -> LogFileData {
     let file = std::fs::File::open("samples/Player.log").unwrap();
     let reader = std::io::BufReader::new(file);
     let mut buff = Vec::with_capacity(INITIAL_SIZE);
     for (line_number, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        buff.push(RawDataEntry {
-            data: line,
-            line_number,
+        buff.push({
+            let mut d = RawDataEntry {
+                data: line,
+                line_number,
+                parsed: None,
+            };
+            d.parse_line(&mut d).unwrap();
+            d
         });
     }
-    buff.par_iter().map(|line| parse_line(line))
-        .map(|entry| {
-            let mut map = BTreeMap::new();
-            map.insert(entry.data().len(), 1);
-            map
-        })
-        .reduce(|| BTreeMap::new(), |mut acc, mut map| {
-            for (key, value) in map.into_iter() {
-                *acc.entry(key).or_insert(0) += value;
-            }
-            acc
-        })
+    let v: Vec<ParsedDataEntry> = buff.into_par_iter().map(|line| parse_line(line)).collect();
+    v.iter().find(|v| {
+        match v {
+            ParsedDataEntry::Unknown(_) => true,
+            _ => false,
+        }
+    }).iter().next().unwrap();
+    LogFileData {
+        metadata: Metadata {},
+        decks: vec![],
+        games: vec![],
+    }
 }
 
 #[derive(Debug)]
 struct RawDataEntry {
     data: String,
     line_number: usize,
+    parsed: Option<ParsedDataEntry>,
 }
 
-enum ParsedDataEntry<'a> {
-    /// Uncategorised data
-    Unknown(&'a RawDataEntry),
-    /// Data that we know is useless
-    Irrelevant(&'a RawDataEntry),
-    /// Data can be JSON representation
-    JSON(&'a RawDataEntry),
-}
+impl RawDataEntry {
+    fn parse_line(&mut self, input: &mut RawDataEntry) -> Result<&Self, ()>{
+        if let Ok(jsoned) = serde_json::from_str(&input.data) {
+            match jsoned {
+                serde_json::Value::Object(_) =>  {
+                    self.parsed = Some(ParsedDataEntry::JSON);
+                    return Ok(self);
+                }
+                serde_json::Value::Array(_) => {
+                    self.parsed=Some(ParsedDataEntry::JSON);
+                    return Ok(self);
+                },
+                _ => panic!("Unexpected JSON type, {}", jsoned),
 
-impl ParsedDataEntry<'_> {
-    fn data(&self) -> &str {
-        match self {
-            ParsedDataEntry::Unknown(entry) => &entry.data,
-            ParsedDataEntry::Irrelevant(entry) => &entry.data,
-            ParsedDataEntry::JSON(entry) => &entry.data,
+            }
         }
+        self.parsed = Some(ParsedDataEntry::Unknown);
+        Err(())
     }
 }
 
-fn parse_line(input: &RawDataEntry) -> ParsedDataEntry{
-    let jsoned = serde_json::from_str(&input.data);
-    if jsoned.is_ok() {
-        return ParsedDataEntry::JSON(input);
-    }
-    ParsedDataEntry::Unknown(input)
+enum ParsedDataEntry {
+    /// Uncategorised data
+    Unknown,
+    /// Data that we know is useless
+    Irrelevant,
+    /// Data can be JSON representation
+    JSON,
 }
+
 
 #[test]
 fn main() {
     tracing_subscriber::fmt::init();
     info!("Started reading");
-    let lines = parse_file();
+    let data = parse_file();
     info!("Finished reading");
-    println!("Number of lines: {}", lines.len());
-    for (k,v) in lines.iter() {
-        println!("{}: {}", k, v);
-    }
 }
